b0e8af70eab22f2c08167ab145b838f43240946b
CJ-Yang <cj.yang0225@gmail.com>
2020-12-07 00:28:59 +0800

    Update main.cpp

--- CPE 49/03. Primary Arithmetic/main.cpp
+++ CPE 49/03. Primary Arithmetic/main.cpp
@@ -1,31 +1,28 @@
-#include <iostream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int a, b;
+
 int main() {
-	int a, b;
+  while (cin >> a >> b) {
+    int count = 0;
+    int carry = 0;
+    
+    if (a == 0 & b == 0) break;
 	
-	while (cin >> a >> b) {
-		if (a == 0 && b == 0) break;
-		
-		int carry = 0, count = 0;
-		
-		while (a != 0 || b != 0) {
-			carry = a % 10 + b % 10 + carry;
-			carry /= 10;
-			a /= 10;
-			b /= 10;
-			
-			if (carry != 0) count++;
-		}		
-		if (count == 0) {
-		cout <<"No carry operation." << endl;
-		} else if (count == 1) {
-			cout << "1 carry operation." << endl;
-		} else {
-			cout << count << " carry operations." << endl;
-		}
-	}
-	return 0;
+    while (a || b) {
+      carry = a % 10 + b % 10 + carry;
+      carry /= 10;
+      a /= 10;
+      b /= 10;
+    
+      if (carry) count++;
+    }
+  
+    if (count == 0) cout << "No carry operation." << endl;
+    else if (count == 1) cout << "1 carry operation." << endl;
+    else cout << count << " carry operations." << endl;
+  }
+  return 0;
 }
