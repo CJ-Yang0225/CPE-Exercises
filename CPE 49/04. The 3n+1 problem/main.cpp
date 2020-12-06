a80138b7df4ce46667d3fd1324b33ddec463530f
CJ-Yang <cj.yang0225@gmail.com>
2020-12-07 00:29:06 +0800

    Update main.cpp

--- CPE 49/04. The 3n+1 problem/main.cpp
+++ CPE 49/04. The 3n+1 problem/main.cpp
@@ -1,43 +1,28 @@
-#include <iostream>
-#include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-	int int1, int2, temp;
-	
-	while (cin >> int1 >> int2) {
-	
-		cout << int1 << " " << int2 << " ";
-		
-		if (int1 > int2) {
-			temp = int1;
-			int1 = int2;
-			int2 = temp;
-		}
-
-		int now, tempLength, maxLength = 0;
-
-		for (int i = int1; i <= int2; i++) {
-			
-			tempLength = 1;
-			now = i;			
-			
-			while (now != 1) {
-				if (now % 2) {
-					now = 3 * now  + 1;
-				} else {
-					now /= 2;
-				}
-				
-				tempLength++;
-			}		
-						
-			if (tempLength > maxLength) {
-				maxLength = tempLength;
-			}
-		}
-		cout << maxLength << endl;
-	}
-	return 0;
+  int n1, n2;
+  
+  while (cin >> n1 >> n2) {
+  	int maxLength = 0, length, now;
+  	
+    cout << n1 << " " << n2 << " ";
+    
+    if (n1 > n2) swap(n1, n2);
+    
+    for (int i = n1; i <= n2; i++) {
+      now = i;
+      length = 1;
+      while (now != 1) {
+        if (now % 2) now = 3 * now + 1;
+        else now /= 2;
+        length++;
+      }
+      if (length > maxLength) maxLength = length;
+    }
+    cout << maxLength << endl;
+  }
+  return 0;
 }
