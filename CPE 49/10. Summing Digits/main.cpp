45bcacd1f299b27cc7624832cc83e8c8a129f217
CJ-Yang <cj.yang0225@gmail.com>
2020-12-12 00:56:17 +0800

    Update main.cpp

--- CPE 49/10. Summing Digits/main.cpp
+++ CPE 49/10. Summing Digits/main.cpp
@@ -1,32 +1,43 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int main() {
+  long long int input;
+    
+  while (cin >> input) {
+    if (input == 0) break;
+    while (input / 10) {
+      input = input % 10 + input / 10;      
+    }
+    cout << input << endl;
+  }
+}
+
+
 void f(string s) {
-		int answer = 0;
-		char buffer[10];
-		
-		for (int i = 0; i < s.size(); i++) {
-			answer += s[i] - '0';
-		}
-		
-		if (s.size() == 1) {
-			cout << answer << endl;
-		} else {
-			itoa(answer, buffer, 10);
-			f(buffer);
-		}
-	}
+  int answer = 0;
+  char buffer[10];
+
+  for (int i = 0; i < s.size(); i++) {
+    answer += s[i] - '0';
+  }
+
+  if (s.size() == 1) {
+    cout << answer << endl;
+  } else {
+    itoa(answer, buffer, 10);
+    f(buffer);
+  }
+}
 
 int main() {
-	string input;
+  string input;
 	
-	while (cin >> input) {
-		if (input == "0") break;	
-		f(input);
-	}
+  while (cin >> input) {
+    if (input == "0") break;	
+    f(input);
+  }
 
-	return 0;
+  return 0;
 }
