f0ecc661f483518918ebfd6cbbaeb643472a95e8
CJ-Yang <cj.yang0225@gmail.com>
2020-12-15 02:10:16 +0800

    Create main.cpp

---
+++ CPE 49/15. Jolly Jumpers/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+      
+  cin.tie(0);
+  while (cin >> n) {
+    int sequence[n], difference[n];
+    memset(difference, 0, sizeof(difference));
+    bool isJollyJumper = true;    
+    
+    cin >> sequence[0];
+    for (int i = 1; i < n; i++) {
+      cin >> sequence[i];
+      difference[abs(sequence[i] - sequence[i - 1])]++;
+    }
+    for (int i = 1; i < n; i++) {
+      if (difference[i] != 1) {
+        isJollyJumper = false;
+        break;      
+      }
+    }
+    if (isJollyJumper) cout << "Jolly\n";
+    else cout << "Not jolly\n";
+  }
+
+  return 0;
+}
