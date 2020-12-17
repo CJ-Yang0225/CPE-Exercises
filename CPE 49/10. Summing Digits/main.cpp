eaf4e441cc64a925b7e2740dd941abd8eba70bcb
CJ-Yang <cj.yang0225@gmail.com>
2020-12-18 03:59:19 +0800

    Update main.cpp

--- CPE 49/10. Summing Digits/main.cpp
+++ CPE 49/10. Summing Digits/main.cpp
@@ -15,29 +15,29 @@ int main() {
 }
 
 
-void f(string s) {
-  int answer = 0;
-  char buffer[10];
+// void f(string s) {
+//   int answer = 0;
+//   char buffer[10];
 
-  for (int i = 0; i < s.size(); i++) {
-    answer += s[i] - '0';
-  }
+//   for (int i = 0; i < s.size(); i++) {
+//     answer += s[i] - '0';
+//   }
 
-  if (s.size() == 1) {
-    cout << answer << endl;
-  } else {
-    itoa(answer, buffer, 10);
-    f(buffer);
-  }
-}
+//   if (s.size() == 1) {
+//     cout << answer << endl;
+//   } else {
+//     itoa(answer, buffer, 10);
+//     f(buffer);
+//   }
+// }
 
-int main() {
-  string input;
+// int main() {
+//   string input;
 	
-  while (cin >> input) {
-    if (input == "0") break;	
-    f(input);
-  }
+//   while (cin >> input) {
+//     if (input == "0") break;	
+//     f(input);
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
