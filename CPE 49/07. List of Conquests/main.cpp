a407eb183aa6a9d3a1a94049e775705e09a0d2ab
CJ-Yang <cj.yang0225@gmail.com>
2020-12-18 03:55:37 +0800

    Update main.cpp

--- CPE 49/07. List of Conquests/main.cpp
+++ CPE 49/07. List of Conquests/main.cpp
@@ -3,36 +3,6 @@
 using namespace std;
 
 int main() {
-  int input, count;
-  
-  cin >> input;
-  cin.ignore(); // 過濾輸入Enter問題
-  
-  string countries[input], name;
-
-  for (int i = 0; i < input; i++) {  	
-    cin >> countries[i];
-    getline(cin, name);    
-  }
-  
-  sort(countries, countries + input); // 將相同國家連續排放
-  
-  int j;
-  for (int i = 0; i < input; i = j) {
-    cout << countries[i] << " "; // 印出目前的國家名
-    count = 0;    
-    for (j = i; j < input; j++) {
-      // 若國家不同，跳出並換到下一個國家(第一個迴圈的 i = j)
-      if (countries[i] != countries[j]) break;
-      count++;
-    }
-    cout << count << endl;
-  }
-  
-  return 0;
-}
-
-int main() {
   map<string, int> countries;
   int n;
   cin >> n;
@@ -50,3 +20,34 @@ int main() {
     cout << iter -> first << " " << iter -> second << endl;
   }
 }
+
+
+// int main() {
+//   int input, count;
+  
+//   cin >> input;
+//   cin.ignore(); // 過濾輸入Enter問題
+  
+//   string countries[input], name;
+
+//   for (int i = 0; i < input; i++) {  	
+//     cin >> countries[i];
+//     getline(cin, name);    
+//   }
+  
+//   sort(countries, countries + input); // 將相同國家連續排放
+  
+//   int j;
+//   for (int i = 0; i < input; i = j) {
+//     cout << countries[i] << " "; // 印出目前的國家名
+//     count = 0;    
+//     for (j = i; j < input; j++) {
+//       // 若國家不同，跳出並換到下一個國家(第一個迴圈的 i = j)
+//       if (countries[i] != countries[j]) break;
+//       count++;
+//     }
+//     cout << count << endl;
+//   }
+  
+//   return 0;
+// }
