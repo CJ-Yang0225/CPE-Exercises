ffb35efb61e2d470f4d731e6d41bc3a9c74b4fa7
CJ-Yang <cj.yang0225@gmail.com>
2020-12-21 00:30:07 +0800

    Create main.cpp

---
+++ CPE 49/38. Can You Solve It/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int loc(int x, int y) {
+  return (x + y) * (x + y + 1) / 2 + x;
+}
+
+int main() {
+  int n, x1, y1, x2, y2, count = 0;
+  
+  cin >> n;  
+  while (n--) {    
+    cout << "Case " << ++count << ": ";
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout << loc(x2, y2) - loc(x1, y1) << endl;
+  }
+  
+  return 0;
+}
