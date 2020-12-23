96ef582cab6cfc289f8b74fc213a885e2b90c341
CJ-Yang <cj.yang0225@gmail.com>
2020-12-23 23:40:11 +0800

    Update main.cpp

--- CPE 49/34. 2 the 9s/main.cpp
+++ CPE 49/34. 2 the 9s/main.cpp
@@ -6,17 +6,15 @@ int main() {
   string input;  
   
   while (cin >> input && input != "0") {
-    int sum = 0;
+    int sum = 0, degree = 1;
     for (int i = 0; i < input.size(); i++) {
       sum += input[i] - '0';
     }
     
-    int degree = 1;
-    
     while (sum > 9 && sum % 9 == 0) {
       int temp = sum;
       sum = 0;
-      
+
       while (temp) {
         sum += temp % 10;
         temp /= 10;        
